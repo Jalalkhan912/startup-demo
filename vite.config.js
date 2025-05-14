101b78891eadfd11414529e9d102e400690c74f6
Jalal Khan <118057500+Jalalkhan912@users.noreply.github.com>
2025-05-14 19:48:47 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: true,
+  },
+})
